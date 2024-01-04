826213f9e0b68b99d9e0eadf301a194c16f04117
NYR4you <132083169+NYR-Solutions@users.noreply.github.com>
2024-01-04 13:13:38 -0600

    Update AppCustomizations.js

--- AppCustomizations.js
+++ AppCustomizations.js
@@ -1,16 +1,1 @@
 //NYR Solutions - Maximo4you.fr
-
-woStatusFilter(item) {
-  // Retourne l'inverse de nos résultats souhaités (éléments à supprimer).
-  return !['WMATL', 'INPRG', 'CUSTOMCOMP'].includes(item.value);
-}
-
-async onAfterLoadData(dataSource, items) {
-  if (dataSource && dataSource.name === "dsstatusDomainList") {
-    // Assurez-vous qu'il y a au moins un enregistrement.
-    if (items && items.length > 0) {
-      let filteredItems = items.filter(this.woStatusFilter);
-      await dataSource.deleteItems(filteredItems);
-    }
-  }
-}
