38e8ec145e8274b8a1c4956188cb35f0cd6a639e
NYR4you <132083169+NYR-Solutions@users.noreply.github.com>
2024-01-04 13:06:44 -0600

    Update AppCustomization.js

--- RestrictionStatuts/AppCustomization.js
+++ RestrictionStatuts/AppCustomization.js
@@ -1,1 +1,16 @@
+//NYR Solutions - Maximo4you.fr
 
+woStatusFilter(item) {
+  // Retourne l'inverse de nos résultats souhaités.
+  return !['WMATL', 'INPRG', 'COMP'].includes(item.value);
+}
+
+async onAfterLoadData(dataSource, items) {
+  if (dataSource && dataSource.name === "dsstatusDomainList") {
+    // Assurez-vous qu'il y a au moins un enregistrement.
+    if (items && items.length > 0) {
+      let filteredItems = items.filter(this.woStatusFilter);
+      await dataSource.deleteItems(filteredItems);
+    }
+  }
+}
