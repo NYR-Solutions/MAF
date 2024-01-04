65fcbd34a3cdd861ce922eb1f0fb4de78b3cba98
NYR4you <132083169+NYR-Solutions@users.noreply.github.com>
2024-01-04 13:04:57 -0600

    Update AppCustomization.js

--- MinuteurWO/AppCustomization.js
+++ MinuteurWO/AppCustomization.js
@@ -1,1 +1,26 @@
+   computedWOTimerDuration(item)
+   {
+     let duration=0;
+     if (item && item.labtrans && item.labtrans.length > 0) {
+       // NOTE: find renverra le premier enregistrement de main-d'œuvre correspondant. En théorie, il devrait toujours y en avoir qu'un seul.
+       let labor=item.labtrans.find((member) => (member.timerstatus_maxvalue === "ACTIVE" && member.laborcode === this.app.client.userInfo.labor.laborcode));
+       if (labor)
+       {
+         let startDateTime=this.app.dataFormatter.convertISOtoDate(labor.startdatetime);
+         let currentDate=this.app.dataFormatter.currentDate();
+         duration=Math.abs((currentDate - startDateTime)) / 3600000;
+       }
+     }
+     return duration;
+   }
+   async onAfterLoadData(dataSource,items)
+   {
+     if (dataSource && dataSource.name=="woDetailResource" && items)
+     {
+       items.forEach((item) => {
+        let duration=this.computedWOTimerDuration(item);
+        item.computedtimerduration=duration;
+       });
+     }
+   }
 
